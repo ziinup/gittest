56431a5efa49e2ec92ee2bc75190f9b74a6dea0a
ziinup <wlsgmlxo@gmail.com>
2024-10-02 10:56:35 +0900

    Initial Commit

---
+++ gittest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  gittest
+//
+//  Created by 진희태 on 2024/10/02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ gittest/gittestApp.swift
@@ -0,0 +1,17 @@
+//
+//  gittestApp.swift
+//  gittest
+//
+//  Created by 진희태 on 2024/10/02.
+//
+
+import SwiftUI
+
+@main
+struct gittestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
